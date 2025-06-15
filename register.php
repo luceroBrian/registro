7abe48c057d21f1f2b522d001de75e01ab6d26e2
luceroBrian <lucerobriandavid1@gmail.com>
2025-06-15 00:07:25 -0300

    subido

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>     
+    <div>
+        <?php 
+            $to = "lucerobriandavid1@gmail.com";
+            $email = $_REQUEST['email']; 
+        ?>
+        <button> 
+            <a href="index.html">
+                regresar
+            </a>
+        </button>
+    </div>
+    
+</body>
+</html>
